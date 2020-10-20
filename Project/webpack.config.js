a27166e373a82578cb152d8df9cc1aacdb62db44
NikChupin <nikchupin91@gmail.com>
2020-10-20 16:53:12 +0500

    some fixes

--- Project/webpack.config.js
+++ Project/webpack.config.js
@@ -3,7 +3,7 @@ const HTMLWebpackPlugin = require('html-webpack-plugin');
 
 const NODE_ENV = process.env.NODE_ENV;
 const IS_DEV = NODE_ENV === 'development';
-const PROD = NODE_ENV === 'production';
+const IS_PROD = NODE_ENV === 'production';
 
 function setupDevtool() {
     if (IS_DEV) return 'eval';
@@ -22,7 +22,7 @@ module.exports = {
     },
     module: {
         rules: [{
-            test: /\.[tj]sx?/,
+            test: /\.[jt]sx?$/,
             use: ['ts-loader']
         }]
     },
